6a32eeb80384a5c24cffef8341e0a61e03699472
Malak <ibrhr55@gmail.com>
2023-07-08 21:30:35 +0300

    from here start the task

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'personalPage.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            body: Center(
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            color: Colors.green,
-            width: 200,
-            height: 200,
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            onLongPress: () {},
-            child: Text('Child'),
-          ),
-        ],
-      ),
-    )));
-  }
-}
+void main() => runApp(const personalPage());
